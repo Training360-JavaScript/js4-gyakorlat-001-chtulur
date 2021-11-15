d6b9acaecd3f5a1dac055bae295f2604fc8c71d1
chtulur <molnar.gly@gmail.com>
2021-11-15 20:23:19 +0100

    Done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,13 @@
+const sorterPromise = (arr) => {
+  const newP = new Promise((resolve, reject) => {
+    if (arr.every((element) => typeof element === "string")) {
+      resolve(arr);
+    } else {
+      reject(`Error: Not all elements are string type!`);
+    }
+  });
 
+  return newP.then((arr) => arr.sort());
+};
 
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise;
